c58c776268b2e1088436157294c572d9e20c7d19
telcaslor <thatbytc@gmail.com>
2025-09-21 18:23:26 +0200

    Commit con el javadoc y el logback

--- src/main/java/es/telmocas/App.java
+++ src/main/java/es/telmocas/App.java
@@ -2,32 +2,55 @@ package es.telmocas;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class App extends Application {
 
+    // Logger para esta clase
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     @Override
-    public void start(Stage stage) throws Exception {
-        // Cargar el archivo FXML
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vista.fxml"));
-        Parent root = loader.load();
+    public void start(Stage stage) {
+        try{
+            logger.debug("Intentando cargar el FXML: /fxml/vista.fxml");
+            FXMLLoader loaded = new FXMLLoader(getClass().getResource("/fxml/vista.fxml"));
+            Scene scene = new Scene(loaded.load());
+            logger.info("FXML cargado correctamente");
 
-        // Crear la escena
-        Scene scene = new Scene(root);
+            // Comprobar que el archivo de CSS existe y si no mostrar una alerta
+            logger.debug("Buscando el archivo CSS");
+            var archivoCSS = getClass().getResource("/css/style.css");
+            if(archivoCSS != null){
+                logger.info("CSS cargado correctamente");
+                scene.getStylesheets().add(archivoCSS.toExternalForm());
+            } else{
+                logger.error("No se ha podido cargar el CSS");
+                new Alert(Alert.AlertType.INFORMATION, "No se ha podido cargar el CSS").showAndWait();
+            }
 
-        // Cargar el CSS
-        scene.getStylesheets().add(getClass().getResource("/estilo.css").toExternalForm());
+            logger.info("Cargar la aplicación");
+            stage.setTitle("GridPane 19092025");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
 
-        // Configurar y mostrar la ventana
-        stage.setTitle("Mi Aplicación JavaFX");
-        stage.setScene(scene);
-        stage.show();
+        } catch (Exception e) {
+            logger.error("Error al intentar cargar la aplicación", e);
+            new Alert(Alert.AlertType.ERROR, "Error al intentar cargar la aplicación").showAndWait();
+        }
+    }
+
+    @Override
+    public void stop(){
+        logger.info("Aplicación finalizada correctamente");
     }
 
     public static void main(String[] args) {
-        launch(args);
-
+        logger.info("Iniciando aplicación JavaFX...");
+        launch();
     }
 }
